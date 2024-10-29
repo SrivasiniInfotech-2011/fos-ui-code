2eca26dd1d30c34a1598d03efc5a80e55c450bd7
Hardik Gondalia <hardikgondalia1990@gmail.com>
2024-10-29 17:23:10 +0530

    validation in login form

--- src/app/authentication/components/login/login.component.ts
+++ src/app/authentication/components/login/login.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  public loginForm: FormGroup;
+  public isSubmitted: boolean = false;
+
+  constructor(private router:Router) {
+    this.loginForm = new FormGroup(
+      {
+        userName: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required])
+      }
+    )
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+  login() {
+    this.isSubmitted = true;
+    if(this.loginForm.valid){
+      this.isSubmitted = false;
+      this.router.navigate(['/dashboard'])
+    }
+  }
 
 }
