ba4773c62eeaed1adccc013546acec8118abf3ec
Hardik Gondalia <hardikgondalia1990@gmail.com>
2024-11-25 13:55:45 +0530

    Update fos-routing.module.ts

--- src/app/modules/dashboard/fos/fos-routing.module.ts
+++ src/app/modules/dashboard/fos/fos-routing.module.ts
@@ -29,10 +29,10 @@ const routes: Routes = [
         path: 'viewloan-details',
         component: ViewloanDetailsComponent,
       },
-      // {
-      //   path: 'prospect-master',
-      //   component: ProspectDetailsComponent
-      // },
+      {
+        path: 'prospect-master',
+        component: ProspectDetailsComponent
+      },
       {
         path: 'lead-master',
         component: LeadMasterComponent,
