bf47331a461ec2ec240150a23c0d0a71ea04110f
Hardik Gondalia <hardikgondalia1990@gmail.com>
2024-12-10 19:09:49 +0530

    changes in admin components

--- src/app/modules/dashboard/admin/pages/company/company-master/company-master.component.ts
+++ src/app/modules/dashboard/admin/pages/company/company-master/company-master.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -8,47 +9,50 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CompanyMasterComponent {
 
-  public companyMasterForm:FormGroup;
-  public corporateDetailsForm:FormGroup;
-  public otherDetailsForm:FormGroup;
+  public companyMasterForm: FormGroup;
+  public corporateDetailsForm: FormGroup;
+  public otherDetailsForm: FormGroup;
 
-  constructor(){
+  constructor(private location: Location) {
     this.companyMasterForm = new FormGroup({
-      companyCode:new FormControl('', [Validators.required]),
-      companyName:new FormControl('', [Validators.required]),
-      constitutionalStatus:new FormControl('', [Validators.required]),
-      city:new FormControl('', [Validators.required]),
-      state:new FormControl('', [Validators.required]),
-      country:new FormControl('', [Validators.required]),
-      pincode:new FormControl('', [Validators.required]),
-      companyAddress:new FormControl('', [Validators.required])
+      companyCode: new FormControl('', [Validators.required]),
+      companyName: new FormControl('', [Validators.required]),
+      constitutionalStatus: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
+      country: new FormControl('', [Validators.required]),
+      pincode: new FormControl('', [Validators.required]),
+      companyAddress: new FormControl('', [Validators.required])
     });
 
     this.corporateDetailsForm = new FormGroup({
-      ceoName:new FormControl('', [Validators.required]),
-      mobileNumber:new FormControl('', [Validators.required]),
-      telephoneNumber:new FormControl('', [Validators.required]),
-      email:new FormControl('', [Validators.required]),
-      website:new FormControl('', [Validators.required]),
-      systemAdminCode:new FormControl('', [Validators.required]),
-      systemAdminPassword:new FormControl('', [Validators.required])
+      ceoName: new FormControl('', [Validators.required]),
+      mobileNumber: new FormControl('', [Validators.required]),
+      telephoneNumber: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      website: new FormControl('', [Validators.required]),
+      systemAdminCode: new FormControl('', [Validators.required]),
+      systemAdminPassword: new FormControl('', [Validators.required])
     });
 
     this.otherDetailsForm = new FormGroup({
-      communicationAddress:new FormControl('', [Validators.required]),
-      address1:new FormControl('', [Validators.required]),
-      city:new FormControl('', [Validators.required]),
-      state:new FormControl('', [Validators.required]),
-      country:new FormControl('', [Validators.required]),
-      pincode:new FormControl('', [Validators.required]),
-      companyStartDate:new FormControl('', [Validators.required]),
-      licenseNumber:new FormControl('', [Validators.required]),
-      validityOfLicenseNumber:new FormControl('', [Validators.required]),
-      incomeTaxNumber:new FormControl('', [Validators.required]),
-      accountingCurrency:new FormControl('', [Validators.required]),
-      remarks:new FormControl('', [Validators.required])
+      communicationAddress: new FormControl('', [Validators.required]),
+      address1: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      state: new FormControl('', [Validators.required]),
+      country: new FormControl('', [Validators.required]),
+      pincode: new FormControl('', [Validators.required]),
+      companyStartDate: new FormControl('', [Validators.required]),
+      licenseNumber: new FormControl('', [Validators.required]),
+      validityOfLicenseNumber: new FormControl('', [Validators.required]),
+      incomeTaxNumber: new FormControl('', [Validators.required]),
+      accountingCurrency: new FormControl('', [Validators.required]),
+      remarks: new FormControl('', [Validators.required])
     })
   }
 
+  back() {
+    this.location.back()
+  }
 
 }
