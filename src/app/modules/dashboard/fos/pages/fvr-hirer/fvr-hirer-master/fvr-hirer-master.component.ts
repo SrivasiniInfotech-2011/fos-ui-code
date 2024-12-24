2dca4a51faf3f0c6bc821c5ae9090ae192c5ed00
Hardik Gondalia <hardikgondalia1990@gmail.com>
2024-12-24 16:26:44 +0530

    added mat-tabs in fvr-hirer-master component

--- src/app/modules/dashboard/fos/pages/fvr-hirer/fvr-hirer-master/fvr-hirer-master.component.ts
+++ src/app/modules/dashboard/fos/pages/fvr-hirer/fvr-hirer-master/fvr-hirer-master.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
@@ -7,10 +7,14 @@ import { ToastrService } from 'ngx-toastr';
   templateUrl: './fvr-hirer-master.component.html',
   styleUrl: './fvr-hirer-master.component.scss'
 })
-export class FvrHirerMasterComponent {
+export class FvrHirerMasterComponent implements OnInit {
 
   public fvrHirerLeadForm:FormGroup;
-  public fvrHirerLeadDetailsForm:FormGroup;
+  public prospectDetailsForm:FormGroup;
+  public fvrObservationLeadForm: FormGroup;
+  public fvrObservationDetailsForm: FormGroup;
+  public fvrNeighbourLeadForm: FormGroup;
+  public fvrNeighbourDetailsForm: FormGroup;
   public isSubmitted:boolean = false;
 
 
@@ -20,7 +24,7 @@ export class FvrHirerMasterComponent {
       vehicleNumber:new FormControl('', [Validators.required])
     });
 
-    this.fvrHirerLeadDetailsForm = new FormGroup({
+    this.prospectDetailsForm = new FormGroup({
       leadNumber : new FormControl(''),
       leadDate : new FormControl(''),
       branch : new FormControl(''),
@@ -29,6 +33,62 @@ export class FvrHirerMasterComponent {
       vehicleNumber : new FormControl(''),
       prospectAddress : new FormControl(''),
     });
+
+    this.fvrObservationLeadForm = new FormGroup({
+      leadNumber: new FormControl(''),
+      vehicleNumber: new FormControl(''),
+      prospectName:new FormControl('')
+    });
+
+    this.fvrObservationDetailsForm = new FormGroup({
+      houseAccessibility: new FormControl(''),
+      locality: new FormControl(''),
+      houseType: new FormControl(''),
+      flooringType: new FormControl(''),
+      roofingType: new FormControl(''),
+      livingStandard: new FormControl(''),
+      entryPermitted: new FormControl(''),
+      houseSize: new FormControl(''),
+      landmark: new FormControl(''),
+      recommend: new FormControl(''),
+      earlierVisit: new FormControl(''),
+      politicalLeaderPhotograph: new FormControl(''),
+      verifierName: new FormControl(''),
+      reason: new FormControl(''),
+      verifierId: new FormControl(''),
+      time: new FormControl(''),
+      branchName: new FormControl(''),
+      dateOfVisit: new FormControl(''),
+      assetInHouse: new FormControl(''),
+      hirerHousePhoto: new FormControl(''),
+    });
+
+    this.fvrNeighbourLeadForm = new FormGroup({
+      leadNumber: new FormControl(''),
+      vehicleNumber: new FormControl(''),
+      prospectName:new FormControl('')
+    });
+
+    this.fvrNeighbourDetailsForm = new FormGroup({
+      hirerLocation: new FormControl(''),
+      yearsOfStay: new FormControl(''),
+      ownershipStatus: new FormControl(''),
+      residenceSubType: new FormControl(''),
+      neighbourName: new FormControl(''),
+      neighbourNumber: new FormControl(''),
+      neighbourAddress: new FormControl(''),
+      comment: new FormControl(''),
+      neighbourHousePhoto: new FormControl(''),
+    });
+
+  }
+
+  ngOnInit(): void {
+    this.prospectDetailsForm.disable();
+    this.fvrObservationLeadForm.disable();
+    this.fvrObservationDetailsForm.disable();
+    this.fvrNeighbourLeadForm.disable();
+    this.fvrNeighbourDetailsForm.disable();
   }
 
   go(){
